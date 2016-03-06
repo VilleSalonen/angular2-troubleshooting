70668035f66e7bdf70dc50b7b8d051e6c5c0bfbc
Ville Salonen <ville.salonen@iki.fi>
2016-03-06 19:04:16 +0200

    Fixed paths

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ var app = express();
 app.route("/api").get((req, res) => {
     res.send("Hello");
 });
-this.app.use(express.static(path.join(__dirname, "client")));
-app.use("/node_modules", express.static(path.join(__dirname, "../node_modules")));
+app.use(express.static(path.join(__dirname, "client")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 app.listen(3000);
